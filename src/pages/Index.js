308d486ded90abffee5e21dd45e73ad071088971
Bijay Kadariya <bijaykadariya92@gmail.com>
2023-10-18 14:16:00 -0400

    final version

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -11,20 +11,21 @@ function Index (props){
    const imagesArray = items.map((item) => item.image);
 
     return<>
-        <Nav/>
-        <div  className="formSale">
-            <div className="carouselContainer">
-                <h2 className='animate-typing text-2xl carouselLabel text-red-800'>Items on Sale !!!</h2>
-                <Carousel images={imagesArray} /> 
+
+            <Nav/>
+            <div  className="formSale">
+                <div className="carouselContainer">
+                    <h2 className='animate-typing text-2xl carouselLabel text-red-800'>Items on Sale !!!</h2>
+                    <Carousel images={imagesArray} /> 
+                </div>
+                <Forms className="indexFormAll"/>     
             </div>
-            <Forms className="indexFormAll"/>     
-        </div>
-            
-        <h1 className="itemList marquee">All Item</h1>
-        <div  className="allItems">
-            {items.map((item)=> {return <Post key = {item.id} item={item}/>})} 
-        </div>
-        <Footer/>
+                
+            <h1 className="itemList marquee">All Item</h1>
+            <div  className="allItems">
+                {items.map((item)=> {return <Post key = {item.id} item={item}/>})} 
+            </div>
+            <Footer/>
 
         
 
